1b3938f2d39b516090d1d0ff09f545f70ec5d39d
Ryan Carvalho Bernardo <106412618+Ryaannsz@users.noreply.github.com>
2025-04-07 16:19:01 -0300

    Update PlayerResults.tsx

--- src/pages/PlayerResults.tsx
+++ src/pages/PlayerResults.tsx
@@ -30,7 +30,7 @@ function PlayerResults() {
           </h2>
           
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {result.map((player: any) => (
+            {result.data.map((player: any) => (
               <div 
                 key={player.id}
                 onClick={() => navigate(`/player/${player.id}`)}
